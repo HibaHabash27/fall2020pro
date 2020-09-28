29e123f025f39582e5ed51f7eb321576a1ccaa5f
2018 <2018@DESKTOP-6M9UI6A>
2020-09-28 19:12:01 +0300

    scenario 13 left

---
+++ src/main/java/home_search/Search.java
@@ -0,0 +1,171 @@
+package home_search;
+
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Search {
+	CopyOnWriteArrayList<home_information> home_result;
+	boolean flag;
+
+	public Search(CopyOnWriteArrayList<home_information> home_array) {
+		home_result = home_array;
+	}
+
+	public void searchByType(String type) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.type.equalsIgnoreCase(type))
+				continue;
+			else
+				home_result.remove(home);
+		}
+
+	}
+
+	public void searchByPlacement(String place) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.placement.equalsIgnoreCase(place))
+				continue;
+			else
+				home_result.remove(home);
+		}
+	}
+
+	public CopyOnWriteArrayList<home_information> printResult() {
+		if (home_result.isEmpty()) {
+			System.out.println("There is no house with such specifications :( ");
+		}
+		else
+		for (home_information home : home_result) {
+			System.out.println(home.type + "-" + home.material + "-" + home.placement);
+			// type [0]*-material [1]* -placement [2]* -pets [3]* - amenties [4] -price [0]
+			// -area [1] -bedrooms [2] -bathrooms [3] -leaselength [4]
+		}
+		return home_result;
+	}
+
+	public void searchByPrice(int price) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.price < price)
+				continue;
+			else
+				home_result.remove(home);
+		}
+	}
+
+	public void searchByMaterial(String string) {
+		for (home_information home : home_result) {
+			if (home.material.equalsIgnoreCase(string))
+				continue;
+			else
+				home_result.remove(home);
+		}
+
+	}
+
+	public void searchByNumberOfBedrooms(int NOB) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.number_of_bedrooms == NOB)
+				continue;
+			else
+				home_result.remove(home);
+		}
+	}
+
+	public void searchByNumberOfBathrooms(int NOB) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.number_of_bathrooms == NOB)
+				continue;
+			else
+				home_result.remove(home);
+		}
+	}
+
+	public void searchBetweenRangeOfPrice(int low_price, int high_price) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.price <= high_price && home.price >= low_price)
+				continue;
+			else
+				home_result.remove(home);
+
+		}
+	}
+
+	public void searchBetweenRangeOfarea(int low_area, int high_area) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.area <= high_area && home.area >= low_area)
+				continue;
+			else
+				home_result.remove(home);
+
+		}
+	}
+
+	public void searchByAreaBelow(int Area) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.area <= Area)
+				continue;
+			else
+				home_result.remove(home);
+
+		}
+	}
+
+	public void searchByAllowingPets(boolean pets) {
+		if (home_result.isEmpty()) return;
+		if (pets = true) {
+			for (home_information home : home_result) {
+				if (home.allowing_pets)
+					continue;
+				else
+					home_result.remove(home);
+			}
+		} else {
+			for (home_information home : home_result)
+				if (home.allowing_pets == false)
+					continue;
+				else
+					home_result.remove(home);
+		}
+
+	}
+
+	public void searchByLeaseLength(int lease_len) {
+		if (home_result.isEmpty()) return;
+		for (home_information home : home_result) {
+			if (home.lease_length == lease_len)
+				continue;
+			else
+				home_result.remove(home);
+		}
+
+	}
+
+	public void searchByAmenities(String amenities) {
+		if (home_result.isEmpty()) return;
+		flag = false;
+		for (home_information home : home_result) {
+			for (String am : home.amenties) {
+				if (amenities.equalsIgnoreCase(am)) {
+					flag = true;
+					break;
+				}
+
+			}
+			if (flag == true)
+				continue;
+			else
+				home_result.remove(home);
+		}
+
+	}
+
+
+}
--- src/test/java/home_search/search_home.java
+++ src/test/java/home_search/search_home.java
@@ -1,272 +1,210 @@
 package home_search;
 
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class search_home {
-	ArrayList<home_information> home_inf = new ArrayList<home_information>();
+	CopyOnWriteArrayList<home_information> home_inf = new CopyOnWriteArrayList<home_information>();
 	String type;
 	int price_less;
 	String placement;
-	String material  ;
-	int number_of_bedroom, lease_len ;
+	String material;
+	String amenities;
+	int number_of_bedroom, lease_len;
 	int number_of_bathroom;
-	int low_price , high_price , home_area , low_area , high_area  ;
-	boolean pets ; 
-   	java.util.List<java.util.List<String>> list;
+	int low_price, high_price, home_area, low_area, high_area;
+	boolean pets;
+	java.util.List<java.util.List<String>> list;
+	Search search;
 
 	@Given("these homes are contained in the system")
 	public void theseHomesAreContainedInTheSystem(io.cucumber.datatable.DataTable dataTable) {
-		//type [0]*-material [1]* -placement [2]* -pets [3]* - amenties [4] -price [0] -area [1] -bedrooms [2] -bathrooms [3] -leaselength [4]
 		
-			 String temp = null;
-		
-			 list=dataTable.asLists();
-			 for(int i=0;i<list.size();i++)
-				  {temp=list.get(i).toString();
-				  java.util.List<String> list2=list.get(i);
-				  String ss=list2.get(0);
-				  String s2=list2.get(1);
-				  String [] tokens=ss.split("_"); 
-				  String [] tokens2=s2.split("_");
-				  home_information home=new home_information();
-					 home.type=tokens[0];
-					 home.material=tokens[1];
-					 home.placement=tokens[2];
-					 if (tokens[3].equalsIgnoreCase("yes"))
-						 home.allowing_pets=true;
-					 else home.allowing_pets= false ;
-					 String[] am = tokens[4].split(",");
-					 home.amenties=am;
-					 home.price=Integer.parseInt(tokens2[0]);
-					 home.area = Integer.parseInt(tokens2[1]);
-					 home.number_of_bedrooms=Integer.parseInt(tokens2[2]);
-					 home.number_of_bathrooms=Integer.parseInt(tokens2[3]);
-					 home.lease_length = Integer.parseInt(tokens2[4]);
-					 home_inf.add(home);
 
-				  }
+		String temp = null;
 
+		list = dataTable.asLists();
+		for (int i = 0; i < list.size(); i++) {
+			temp = list.get(i).toString();
+			java.util.List<String> list2 = list.get(i);
+			String ss = list2.get(0);
+			String s2 = list2.get(1);
+			String[] tokens = ss.split("_");
+			String[] tokens2 = s2.split("_");
+			home_information home = new home_information();
+			home.type = tokens[0];
+			home.material = tokens[1];
+			home.placement = tokens[2];
+			if (tokens[3].equalsIgnoreCase("yes"))
+				home.allowing_pets = true;
+			else
+				home.allowing_pets = false;
+			String[] am = tokens[4].split(",");
+			home.amenties = am;
+			home.price = Integer.parseInt(tokens2[0]);
+			home.area = Integer.parseInt(tokens2[1]);
+			home.number_of_bedrooms = Integer.parseInt(tokens2[2]);
+			home.number_of_bathrooms = Integer.parseInt(tokens2[3]);
+			home.lease_length = Integer.parseInt(tokens2[4]);
+			home_inf.add(home);
+			search = new Search(home_inf);
+
+		}
 
 	}
 
 	@When("I search about home by {string}")
 	public void iSearchAboutHomeBy(String string) {
-		type = string;
+		search.searchByType(string);
 
 	}
 
 	@Then("A list of homes that matches the  type specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesTheTypeSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.type.equalsIgnoreCase(type))
-				System.out.print(home.placement);
-		}
+		search.printResult();
 
 	}
 
 	@When("I search about home with price less than {int}")
 	public void iSearchAboutHomeWithPriceLessThan(Integer int1) {
 
-		price_less = int1;
+		search.searchByPrice(int1);
 	}
 
 	@Then("A list of homes that matches the price specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesThePriceSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.price < price_less)
-				System.out.print(home.placement);
-		}
+		search.printResult();
 	}
 
 	@When("i search about home by_placement {string}")
 	public void iSearchAboutHomeByPlacement(String string) {
-		placement = string;
+		search.searchByPlacement(string);
 	}
 
 	@Then("A list of homes that matches the  placement specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesThePlacementSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.placement.equalsIgnoreCase(placement))
-				System.out.print(home.placement);
-		}
+		search.printResult();
 	}
 
 	@When("I search about home by_material {string}")
 	public void iSearchAboutHomeByMaterial(String string) {
-		material = string;
+		search.searchByMaterial(string);
 	}
 
 	@Then("A list of homes that matches the  material specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesTheMaterialSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.material.equalsIgnoreCase(material))
-				System.out.print(home.placement);
-		}
+		search.printResult();
 	}
 
 	@When("I search about home by_number_of_bedroom {string}")
 	public void iSearchAboutHomeByNumberOfBedroom(String string) {
 		number_of_bedroom = Integer.parseInt(string);
+		search.searchByNumberOfBedrooms(number_of_bedroom);
 	}
 
 	@Then("A list of homes that matches the  number_of_bedroom specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesTheNumberOfBedroomSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.number_of_bedrooms == number_of_bedroom)
-				System.out.print(home.placement);
-		}
+		search.printResult();
 	}
 
 	@When("I search about home by_number_of_bathroom {string}")
 	public void iSearchAboutHomeByNumberOfBathroom(String string) {
 		number_of_bathroom = Integer.parseInt(string);
+		search.searchByNumberOfBathrooms(number_of_bathroom);
 
 	}
 
 	@Then("A list of homes that matches the  number_of_bathroom specification should be returned and printed on the console")
 	public void aListOfHomesThatMatchesTheNumberOfBathroomSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-		for (home_information home : home_inf) {
-			if (home.number_of_bathrooms == number_of_bathroom)
-				System.out.print(home.placement);
-		}
+		search.printResult();
 	}
-	
-	
-		@When("I search about home between {int} and {int}")
-		public void iSearchAboutHomeBetweenAnd(Integer int1, Integer int2) {
-		    
-		    low_price = int1; 
-		    high_price = int2;
-		}
 
+	@When("I search about home between {int} and {int}")
+	public void iSearchAboutHomeBetweenAnd(Integer int1, Integer int2) {
+		low_price = int1;
+		high_price = int2;
+		search.searchBetweenRangeOfPrice(low_price, high_price);
 
-		
+	}
 
-		@Then("A list of homes that matches the  range of price specification should be returned and printed on the console")
-		public void aListOfHomesThatMatchesTheRangeOfPriceSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-			for (home_information home : home_inf) {
-				if (home.price <= high_price && home.price>= low_price)
-					System.out.print(home.placement);
-		}
-		}
-		
-		
+	@Then("A list of homes that matches the  range of price specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheRangeOfPriceSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
+	}
 
-			@When("I search home below area {int}")
-			public void iSearchHomeBelowArea(Integer int1) {
-			    home_area = int1; 
-			}
+	@When("I search home below area {int}")
+	public void iSearchHomeBelowArea(Integer int1) {
+		search.searchByAreaBelow(int1.intValue());
+	}
 
+	@Then("A list of homes that matches the area specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheAreaSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
+	}
 
-			
+	@When("I search about home by_Alolowing pets {string}")
+	public void iSearchAboutHomeByAlolowingPets(String string) {
+		if (string.equalsIgnoreCase("yes"))
+			pets = true;
+		else
+			pets = false;
+		search.searchByAllowingPets(pets);
+	}
 
-			@Then("A list of homes that matches the area specification should be returned and printed on the console")
-			public void aListOfHomesThatMatchesTheAreaSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-				for (home_information home : home_inf) {
-					if (home.area <= home_area )
-						System.out.print(home.placement);
-			}}
-			
+	@Then("A list of homes that matches the  Allowing pets specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheAllowingPetsSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
 
-				@When("I search about home by_Alolowing pets {string}")
-				public void iSearchAboutHomeByAlolowingPets(String string) {
-				   if (string.equalsIgnoreCase("yes"))
-					   pets = true ;
-				   else pets = false ; 
-				}
+	}
 
+	@When("I search about home area between {int} and {int}")
+	public void iSearchAboutHomeAreaBetweenAnd(Integer int1, Integer int2) {
+		low_area = int1;
+		high_area = int2;
+		search.searchBetweenRangeOfarea(low_area, high_area);
 
-				
+	}
 
-				@Then("A list of homes that matches the  Allowing pets specification should be returned and printed on the console")
-				public void aListOfHomesThatMatchesTheAllowingPetsSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-						if (pets = true  )
-							{for (home_information home : home_inf) {
-								if (home.allowing_pets  )
-									System.out.print(home.placement);}}
-						else 
-							{for (home_information home : home_inf)
-								if (home.allowing_pets==false)
-								System.out.print(home.placement);}
-							
-				}
-				
-				
-				
-				
-				
-					@When("I search about home area between {int} and {int}")
-					public void iSearchAboutHomeAreaBetweenAnd(Integer int1, Integer int2) {
-					   low_area = int1; 
-					   high_area = int2 ;
-					}
+	@Then("A list of homes that matches the  range of area specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheRangeOfAreaSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
 
+	}
 
-					
-					@Then("A list of homes that matches the  range of area specification should be returned and printed on the console")
-					public void aListOfHomesThatMatchesTheRangeOfAreaSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-						for (home_information home : home_inf) {
-							if (home.area <= high_area && home.area>= low_area)
-								System.out.print(home.placement);
-						
-					}}	
-					
-					
-					
+	@When("I search about home by lease length {string}")
+	public void iSearchAboutHomeByLeaseLength(String string) {
+		if (string.equalsIgnoreCase("short"))
+			lease_len = 6;
+		else
+			lease_len = 12;
+		search.searchByLeaseLength(lease_len);
 
-						@When("I search about home by lease length {string}")
-						public void iSearchAboutHomeByLeaseLength(String string) {
-						    if (string.equalsIgnoreCase("short"))
-						       lease_len= 6;
-						    else lease_len = 12 ;
-						    
-						}
+	}
 
+	@Then("A list of homes that matches the  lease length specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheLeaseLengthSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
+	}
 
+	@When("I search about home by Amenities {string}")
+	public void iSearchAboutHomeByAmenities(String string) {
+		amenities = string;
+		search.searchByAmenities(amenities);
 
-						@Then("A list of homes that matches the  lease length specification should be returned and printed on the console")
-						public void aListOfHomesThatMatchesTheLeaseLengthSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-							for (home_information home : home_inf) {
-								if (home.lease_length==lease_len)
-									System.out.print(home.placement);
-						}}
+	}
 
+	@Then("A list of homes that matches the  Amenities specification should be returned and printed on the console")
+	public void aListOfHomesThatMatchesTheAmenitiesSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
+		search.printResult();
 
+	}
 
-							@When("I search about home by Amenities {string}")
-							public void iSearchAboutHomeByAmenities(String string) {
-							
-							}
-
-
-
-							@Then("A list of homes that matches the  Amenities specification should be returned and printed on the console")
-							public void aListOfHomesThatMatchesTheAmenitiesSpecificationShouldBeReturnedAndPrintedOnTheConsole() {
-							   
-							   
-							}
-
-
-
-					
-					
-					
-					
-					
 }
-
-
-
-
-
-
-
 
 
 
